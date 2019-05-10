b2e5004668eda380fc946f60b050226fcfa27d13
Ben Acland <benacland@gmail.com>
2019-05-10 12:16:24 -0500

    neaten setup.py

--- python_modules/blp_trace/setup.py
+++ python_modules/blp_trace/setup.py
@@ -3,7 +3,25 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-        name= "blp_trace",
-        ext_modules = cythonize(Extension('blp_trace',sources=["blp_trace.pyx"],export_symbols=['pluginStartup','pluginisready','getParamNum','getParamConfig','pluginFunction','eventFunction','spikeFunction','setIntParam','setFloatParam','getIntParam','getFloatParam'])),
-        include_dirs = [numpy.get_include()]
-        )
+    name= "blp_trace",
+    ext_modules = cythonize(
+        Extension(
+            'blp_trace',
+            sources=["blp_trace.pyx"],
+            export_symbols=[
+                'pluginStartup',
+                'pluginisready',
+                'getParamNum',
+                'getParamConfig',
+                'pluginFunction',
+                'eventFunction',
+                'spikeFunction',
+                'setIntParam',
+                'setFloatParam',
+                'getIntParam',
+                'getFloatParam',
+                ],
+            )
+        ),
+    include_dirs = [numpy.get_include()]
+    )
