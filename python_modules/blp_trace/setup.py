d040ccad96327754f3f4d1857292e85bdd9f3a26
Ben Acland <benacland@gmail.com>
2019-06-14 10:27:38 -0500

    update setup

--- python_modules/blp_trace/setup.py
+++ python_modules/blp_trace/setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import runpy
+
+cfg = runpy.run_path('../.config.py')
 
 setup(
     name= "blp_trace",
+    include_dirs=[numpy.get_include(), cfg['PYTHON_PLUGIN_SRC_DIR']],
     ext_modules = cythonize(
         Extension(
             'blp_trace',
@@ -21,7 +25,7 @@ setup(
                 'getIntParam',
                 'getFloatParam',
                 ],
-            )
+            ),
+        language_level=3,
         ),
-    include_dirs = [numpy.get_include()]
     )
